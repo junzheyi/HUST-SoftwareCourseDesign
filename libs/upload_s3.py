a16706224c0605af2ad142dbac7773e30d6343a8
Junzhe Yi <149055289+junzheyi@users.noreply.github.com>
2024-12-27 15:33:08 +0800

    upload libs file

--- libs/upload_s3.py
+++ libs/upload_s3.py
@@ -7,23 +7,14 @@
 import boto3
 from botocore.exceptions import ClientError
 from os.path import basename
-# Make it lib.read_config after unit testing.
 from libs.read_config import ReadConfig
 
 
 def upload_file(filename):
     try:
         my_config = ReadConfig("config/config.ini")
-        # Taking the filename as object name
-        # file_object = basename(filename)
-        # Creating an AWS session
         session = boto3.Session(profile_name=my_config.aws_profile_name)
-        # Creating a S3 client
         s3_client = session.client("s3")
-        # response = s3_client.upload_file(Filename=filename,
-        #                              Bucket=my_config.s3_bucket,
-        #                              Key=file_object
-        #                              )
         response = s3_client.upload_fileobj(filename, Bucket=my_config.s3_bucket,
                                             Key=filename.name
                                             )
