cf0d6ad51b3680333edf9b64b7fdb3346e4c29a9
Junzhe Yi <149055289+junzheyi@users.noreply.github.com>
2025-01-07 01:07:56 +0800

    add zephyr(free model in hf)

--- ask_questions_v3.py
+++ ask_questions_v3.py
@@ -4,6 +4,7 @@
 # @File    : ask_questions_v3.py
 # @Software: PyCharm
 
+
 import timeit
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Milvus
@@ -12,6 +13,7 @@ from langchain.chains import RetrievalQA
 from libs import qa_template_zephyr
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import HuggingFaceHub
+
 
 
 def set_qa_prompt():
@@ -56,3 +58,45 @@ def ask_question_zephyr(query_text):
         vector_field="book_chunk_vec",
         consistency_level="Session"
     )
+
+    retriever = vector_db.as_retriever(search_kwargs={'k': top_k,
+                                                      "additional": ["certainty"],
+                                                      "score_threshold": 0.9
+                                                      },
+                                       search_type="similarity"
+                                       )
+    qa_prompt = set_qa_prompt()
+
+    zephyr_llm = HuggingFaceHub(repo_id="HuggingFaceH4/zephyr-7b-beta",
+                                model_kwargs={"temperature": 0.001,
+                                              "max_new_tokens": 512,
+                                              "repetition_penalty": 1.1,
+                                              "max_length": 64,
+                                              "top_p": 0.9,
+                                              "return_full_text": False
+                                              },
+                                huggingfacehub_api_token=my_config.hf_api_token
+                                )
+    dbqa = build_retrieval_qa(zephyr_llm, qa_prompt, retriever)
+    llm_response = dbqa.invoke(query_text)
+    return llm_response
+
+
+if __name__ == '__main__':
+    query = input("Please enter the question: ")
+    start_time = timeit.default_timer()  # Start timer
+    response = ask_question_zephyr(query)
+
+    # Displaying the outcome.
+    print(f'\n\033[92m Answer: \033[0;0m {response["result"]}')
+    print("\033[38;5;208m*******************************\033[0;0m")
+    source_docs = response['source_documents']
+    for i, doc in enumerate(source_docs):
+        print(f'\nSource Document {i + 1}\n')
+        print(f'Source Text: {doc.page_content}')
+        print("\033[38;5;208m*******************************\033[0;0m")
+    print('=' * 50)
+    end_time = timeit.default_timer()  # End timer
+    total_time = (end_time - start_time) / 60
+    print("Time to retrieve response %.2f(minute(s)):" % total_time)
+
